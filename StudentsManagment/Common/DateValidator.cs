3783727930a0ea90533e99b03ef90db2b571894c
matant <matant86@gmail.com>
2016-02-24 16:56:49 +0200

    my first asp.net mvc app

---
+++ StudentsManagment/Common/DateValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentsManagment.Common
+{
+    public class DateValidator : ValidationAttribute
+    {
+        /*
+            Function that check the date the user insert relation to the current date
+            if the difference (calculated by years) is less than 5 return false 
+            otherwise return true.
+        */
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime = Convert.ToDateTime(value);
+            int diff = DateTime.Now.Year - dateTime.Year;
+            if (diff >= 5)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ StudentsManagment/Controllers/HomeController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using StudentsManagment.Models;
+using System.Data.Entity;
+using System.Net;
+using PagedList;
+
+namespace StudentsManagment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private studentsDBEntities _db = new studentsDBEntities();
+        
+        // GET: Home
+        public ActionResult Index(int? page)
+        {
+            return View(_db.students.ToList().ToPagedList(page ?? 1,2));
+        }
+
+        // GET: /Home/Create
+        public ActionResult Create()
+        {         
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(student studentToCreate)
+        {
+            Boolean cityValidator = isCityIsValid(studentToCreate.City);
+
+            if (!ModelState.IsValid ||!cityValidator)
+            {
+                if (!cityValidator)
+                    ModelState.AddModelError("City", studentToCreate.City + " " + "is not a valid city name!");
+                return View(studentToCreate);
+            }          
+            _db.students.Add(studentToCreate);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public JsonResult AutoCompleteCity(string term)
+        {
+
+            var result = (from r in _db.Cities
+                         
+                          where r.CityName.StartsWith(term)
+                          select new { r.CityName }).Distinct();
+            
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        //GET:/Home/Edit
+        public ActionResult Edit(int id)
+        {
+            var studentToEdit = (from s in _db.students
+                                 where s.Id == id
+                                 select s).First();
+            return View(studentToEdit);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(student studentToEdit)
+        {
+            Boolean cityValidator = isCityIsValid(studentToEdit.City);
+
+            if (ModelState.IsValid && cityValidator)
+            {
+                _db.Entry(studentToEdit).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            if(!cityValidator)
+                ModelState.AddModelError("City", studentToEdit.City+" "+"is not a valid city name!");
+            return View(studentToEdit);
+
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            student personalDetail = _db.students.Find(id);
+            if (personalDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personalDetail);
+        }
+
+        // POST: /Home/Delete/id
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            student personalDetail = _db.students.Find(id);
+            _db.students.Remove(personalDetail);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        /*Function that get a city name as an input and check if the input
+          is exist in the cities table.                   
+        */
+        public Boolean isCityIsValid(String city)
+        {
+            var result = (from r in _db.Cities
+
+                          where r.CityName.Equals(city)
+                          select new { r.CityName }).Distinct();
+            if(result.Count() < 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
